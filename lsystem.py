5528afce3d05d876d400d149d2f89699869800c4
mehdialmoo <mehdialmousavie@outlook.com>
2023-11-09 11:52:01 +0000

    lsystemfile has been added

---
+++ lsystem.py
@@ -0,0 +1,27 @@
+from typing import Dict,List
+from pen import Pen
+
+class LSystem():
+
+    rules: dict[str,str]    
+
+    def set_rules(self,rules: Dict[str,str]) -> None:
+        self.rules = rules
+
+    def generate_next_state(self,current_state) -> str:
+        new_state = ""
+        for character in current_state:
+            if character in self.rules.keys():
+                new_state += self.rules[character]
+            else:
+                new_state += character
+        return new_state
+
+class Ltree(LSystem):
+    colour : str 
+    thickness : int
+    length : int
+    angle : int
+    Pen : Pen
+    drawing_stack : list[Dict]
+    
